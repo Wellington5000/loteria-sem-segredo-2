418077dfbdd2bdce0ec4fb045d27dac1aa7542aa
Wellington Teixeira <wellington@tartigrado.com>
2023-11-27 01:25:06 -0300

    add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+declare const gsap: any;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,41 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'loteria-sem-segredo-2';
+
+  constructor() { }
+
+  ngAfterViewInit(): void {
+    const tl = gsap.timeline({ default: { duration: 0.5}});
+
+    const amazon = document.querySelector('.amazon');
+    const americanas = document.querySelector('.americanas');
+    const shopee = document.querySelector('.shopee');
+    const clube = document.querySelector('.clube');
+    const barnes = document.querySelector('.barnes');
+    const estante = document.querySelector('.estante');
+    const shoptime = document.querySelector('.shoptime');
+    const lessDelay = "-=0.4";
+
+    tl.fromTo(amazon,
+      { opacity: 0, y: 100 },
+      { opacity: 1, y: 0 })
+    .fromTo(americanas,
+      { opacity: 0, y: 100 },
+      { opacity: 1, y: 0 }, lessDelay)
+    .fromTo(shopee,
+      { opacity: 0, y: 100 },
+      { opacity: 1, y: 0 }, lessDelay)
+    .fromTo(clube,
+      { opacity: 0, y: 100 },
+      { opacity: 1, y: 0 }, lessDelay)
+    .fromTo(barnes,
+      { opacity: 0, y: 100 },
+      { opacity: 1, y: 0 }, lessDelay)
+    .fromTo(estante,
+      { opacity: 0, y: 100 },
+      { opacity: 1, y: 0 }, lessDelay)
+    .fromTo(shoptime,
+      { opacity: 0, y: 100 },
+      { opacity: 1, y: 0 }, lessDelay)
+  }
 }
